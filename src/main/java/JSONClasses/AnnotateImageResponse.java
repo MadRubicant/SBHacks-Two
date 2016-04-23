232f97143584e189506890fa9dc17222a48fce99
Sierra Schwellenbach <sschwellenbach@umail.ucsb.edu>
2016-04-22 22:49:34 -0700

    Added annotatedImageResponse class

---
+++ src/main/java/JSONClasses/AnnotateImageResponse.java
@@ -0,0 +1,15 @@
+package JSONClasses;
+
+/**
+ * Created by sierra on 4/22/16.
+ */
+public class AnnotateImageResponse {
+    /*
+    EntityAnnotation labelAnnotations;
+    EntityAnnotation textAnnotations;
+    EntityAnnotation landmarkAnnotations;
+    EntityAnnotation logoAnnotations;
+    ImageProperties imagePropertiesAnnotation;
+    Status error;
+    */
+}
--- src/main/java/JSONClasses/Feature.java
+++ src/main/java/JSONClasses/Feature.java
@@ -4,6 +4,6 @@ package JSONClasses;
  * Created by sierra on 4/22/16.
  */
 public class Feature {
-     String type;
+    String type;
     int maxResults;
 }
