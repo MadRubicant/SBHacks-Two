7247a659ce12cab946119ec3faaae1fd1132b7cb
William Bennett <williambennett2014@gmail.com>
2016-04-22 21:54:11 -0700

    Added ImageAnalyzer and ImageData classes

---
+++ src/main/java/ImageAnalyzer.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+/**
+ * Created by William Bennett on 4/22/2016.
+ */
+public class ImageAnalyzer {
+    public Color PillColor(BufferedImage Image ) {
+        int width = Image.getWidth();
+        int height = Image.getHeight();
+        int[] ColorData = new int[width * height];
+        return new Color(0);
+    }
+}
+
---
+++ src/main/java/ImageData.java
@@ -0,0 +1,9 @@
+/**
+ * Created by William Bennett on 4/22/2016.
+ */
+public class ImageData {
+    public int[][] Color2d;
+    public int Width;
+    public int Height;
+    
+}
