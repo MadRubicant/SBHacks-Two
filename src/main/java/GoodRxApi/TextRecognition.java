43c6a4ac49ad048ba5dbf71b354e29b21ffa3613
Sierra Schwellenbach <sschwellenbach@umail.ucsb.edu>
2016-04-23 23:25:28 -0700

    Returns something from TextRecognition... broken but it returns something for caitlin to work with

--- src/main/java/GoodRxApi/TextRecognition.java
+++ src/main/java/GoodRxApi/TextRecognition.java
@@ -3,24 +3,29 @@ package GoodRxApi;
 import com.google.api.services.vision.v1.model.EntityAnnotation;
 
 import java.util.List;
-import GoodRxApi.JSONClasses.*;
-import java.lang.*;
 
+import GoodRxApi.JSONClasses.*;
+
+import java.lang.*;
 
 
 /**
  * Created by sierra on 4/23/16.
  */
 public class TextRecognition {
-    public static void processTextWithGoodRx(List<EntityAnnotation> textAnnotations){
+    public static GoodRxResponse processTextWithGoodRx(List<EntityAnnotation> textAnnotations) {
+        GoodRxResponse goodRxResponse;
         for(EntityAnnotation textInstance : textAnnotations) {
             String textIn = "" + textInstance.get("description");
             System.out.println("Text:\n" + textIn);
-
+            //textIn = textIn.replace('\n', ' ');
 
             GoodRxApiCaller goodRxApiCaller = new GoodRxApiCaller(textIn, "any", "any");
-            GoodRxResponse goodRxResponse = goodRxApiCaller.sendApiRequest(1);
+            goodRxResponse = goodRxApiCaller.sendApiRequest(1);
+            if(goodRxResponse != null)
+                return goodRxResponse;
         }
+        return null;
     }
 
 }
